6e1091630e3e9bc8a22ca6020f3b8ccec1800482
LucasFernandez111 <flucasyoel@outlook.es>
2024-03-15 21:01:57 -0300

    ADD:Handle error - Response

--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,6 +1,7 @@
 import { Response } from "express";
 
-const handleHttp = (res:Response,error:string):void =>{
+const handleHttp = (res:Response,error:string,errorRaw?:any ):void =>{
+    console.info(errorRaw)
 
     res.status(500).send({error})
 
